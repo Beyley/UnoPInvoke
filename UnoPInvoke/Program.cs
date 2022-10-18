6df2d7386f326408fac51090633cf4b0ef571441
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-18 09:25:24 -0700

    Initial commit

---
+++ UnoPInvoke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnoPInvoke; 
+
+public class Program {
+	[DllImport("__Internal_emscripten")]
+	public static extern void emscripten_console_log(string str);
+	
+	public static void Main(string[] args) {
+		emscripten_console_log("teststststs");
+	}
+}
