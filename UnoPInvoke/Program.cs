37f436cd830d707166bc0b31f4c6a591cd2477ba
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-24 23:00:24 -0700

    Silk + Update

--- UnoPInvoke/Program.cs
+++ UnoPInvoke/Program.cs
@@ -1,20 +1,30 @@
 ﻿using System.Runtime.InteropServices;
+using Silk.NET.SDL;
+using Uno.Foundation;
 
 namespace UnoPInvoke; 
 
 public class Program {
-	[DllImport("SDL")]
-	public static extern int SDL_Init(int flags);
-	
-	[DllImport("SDL", CharSet = CharSet.Ansi)]
-	public static extern IntPtr SDL_CreateWindow(string title, int x, int y, int w, int h, int flags);
-	
-	public static void Main(string[] args) {
-		Console.WriteLine($"please... {SDL_Init(0)}");
+	public static unsafe void Main(string[] args) {
+		Sdl sdl = Sdl.GetApi();
 
-		IntPtr window = SDL_CreateWindow("omg", 0, 0, 800, 600, 0);
+		WebAssemblyRuntime.InvokeJS(@"canvas = document.createElement('canvas');
+canvas.style.position = ""absolute"";
+canvas.style.left       = ""0px"";
+canvas.style.top        = ""0px"";
+canvas.style.zIndex     = ""100"";
+canvas.style.width      = ""1920"";
+canvas.style.height     = ""1080"";
+canvas.style.width = ""100%"";
+canvas.style.height = ""100%"";
+canvas.oncontextmenu=""event.preventDefault()"";
+canvas.id = 'canvas';
+document.body.appendChild(canvas);");
 		
+		Console.WriteLine($"please... {sdl.Init(0)}");
+
+		Window* window = sdl.CreateWindow("omg", 0, 0, 800, 600, 0);
 		
-		while(true) {}
+		sdl.Quit();
 	}
 }
